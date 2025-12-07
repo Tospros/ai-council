b4dc9eade6f26f81eee5d30134df7984a432bd36
Antoni Zgajewski <antek004@gmail.com>
2025-12-07 23:16:52 +0100

    chore: Adding schemas

---
+++ schemas/history.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class HistoryBase(BaseModel):
+    prompt: str
+    response: Optional[str]
+
+
+class HistoryCreate(HistoryBase):
+    llm_ids: List[int] = []
+
+
+class HistoryRead(HistoryBase):
+    id: int
+    llms: List[str] = []
+
+    class Config:
+        orm_mode = True
---
+++ schemas/llm.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class LlmBase(BaseModel):
+    name: str
+
+
+class LlmCreate(LlmBase):
+    pass
+
+
+class LlmRead(LlmBase):
+    id: int
+    history: Optional[List[str]] = []
+
+    class Config:
+        orm_mode = True
